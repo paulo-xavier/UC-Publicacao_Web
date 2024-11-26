3823765eed702fb592efa377ef6fc5bc68eb63be
paulo-xavier <px568661@gmail.com>
2024-11-25 21:56:07 -0300

    :lipstick: feat: Palindrome exercise

---
+++ Exercises/3-Teste-unitario/src/3-palindromo/palindromo.js
@@ -0,0 +1,36 @@
+/* Exercício prático: Palíndromo
+
+expect(isPalindrome('Ana')).toBeEqual(true);
+
+1. Implementar a função isPalindrome(string);
+
+2. Implementar os requisitos abaixo e testar;
+    - Reconhece palíndromos em strings simples;
+    - Reconhece palíndromos com letras maiúsculas e minúsculas;
+    - REconhece palíndromos com espaços e pontuação; (Após:a:sopa)
+
+3. Teste
+    - Função que se previne contra strings vazias, nulas, etc.
+
+
+DICA: Implementar como TDD (primeiro teste, depois implementa)
+
+*/
+
+
+
+function isPalindrome(string) {
+    
+    const normalArray = ['string'];
+
+
+
+    const inverterdArray = [];
+
+
+
+
+}
+
+
+module.exports = { isPalindrome };
---
+++ Exercises/3-Teste-unitario/src/3-palindromo/palindromo.test.js
@@ -0,0 +1,12 @@
+const { isPalindrome } = require('./palindromo');
+
+// test('Multiply by zero', () => {
+//     expect(multiply(5,0)).toBe(0);
+// });
+
+
+test('Discover Palindromos in simple strings', () => {
+    expect(isPalindrome('ana')).toBeTruthy(true);
+});
+
+
